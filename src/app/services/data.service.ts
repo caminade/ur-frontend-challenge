e3fe38ab77382ef6508100d8d8aca7471ba55595
Salaheddine <salah.roida@gmail.com>
2019-09-12 13:00:54 +0100

    Update data.service.ts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -10,13 +10,13 @@ import {RepoService} from './repo.service';
 export class DataService {
 
   public repos;
-  private _URL = 'https://api.github.com/search/repositories?q=created:>2019-05-07&sort=stars&order=desc&per_page=100'; //using github api to retrieve the recently created repositories sorted by number of stars
+  private _URL = 'https://api.github.com/search/repositories?q=created:>2019-08-012&sort=stars&order=desc&per_page=100'; //using github api to retrieve the recently created repositories sorted by number of stars
 
   constructor(private http: HttpClient, private slrepo: RepoService) { }
   
     getURL(page: number)  //instead of using _URL we define a method to increment the pages number when scrolling down
     {  
-        return 'https://api.github.com/search/repositories?q=created:>2019-05-07&sort=stars&order=desc&page='+page;  
+        return 'https://api.github.com/search/repositories?q=created:>2019-08-12&sort=stars&order=desc&page='+page;  
     }  
 //    getRepos() {           //Return 100 results without pagination
 //    this.http.get<any>(this._URL, {
